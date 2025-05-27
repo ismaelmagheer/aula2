d33384949103686951120961928951d95c459331
ismael <grossmayara@gmail.com>
2025-05-27 20:44:19 -0300

    first commit

---
+++ exercicio3.php
@@ -0,0 +1,12 @@
+<?php
+$clarinete=900;
+$percentual1=5;
+$percentual2=50;
+$resultado1;
+$resultado2;
+
+$resultado1 = ($clarinete * $percentual1) / 100;
+$resultado2 = ($clarinete * $percentual2) / 100;
+
+echo "O resultado de 5% é: $resultado1\n";
+echo "O resultado de 50% é: $resultado2";
