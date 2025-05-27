e18d87549bf5c846b576fe60b3b60d5b9cd6e8a7
ismael <grossmayara@gmail.com>
2025-05-27 20:52:52 -0300

    first commit

---
+++ exercicio4.php
@@ -0,0 +1,12 @@
+<?php
+$numero=5;
+$numero2=10;
+$quadrado1;
+$quadrado2;
+$soma;
+
+$quadrado1 = $numero * $numero;
+$quadrado2 = $numero2 * $numero2;
+$soma = $quadrado1 + $quadrado2;
+
+echo "O resultado é $soma";
